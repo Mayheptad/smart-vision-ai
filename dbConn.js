ab82c5021f617ffe972cfbf0ef4dc0fb0e780f6b
mayheptad <wfoluwarichie@gmail.com>
2021-11-27 22:40:55 +0100

    still fixing database connection issue

--- dbConn.js
+++ dbConn.js
@@ -1,14 +1,25 @@
+
+
 exports.dbConn = {
   client: "pg",
   connection: {
   connectionString: process.env.DATABASE_URL,
-  ssl: true
- /*  ssl: {
+  ssl: {
     rejectUnauthorized: false
-  } */
+      }
+}
+  }
+
+
 /*     host: "127.0.0.1",
     user: "postgres",
     password: "postgres",
     database: "smartbrain", */
+	
+	
+	{
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 }

f5b9b27816c78ab5a0ed3e643a279af2c86760f3
mayheptad <wfoluwarichie@gmail.com>
2021-11-27 14:51:09 +0100

    change the server response when user visit the home route

--- Controller/home.js
+++ Controller/home.js
@@ -3,7 +3,8 @@
 
 
 exports.handleHomeRoute = knex => (req, res) => 
-  knex.select("*")
-    .from("users")
-    .then( user => res.json(user))
-    .catch( _ => res.status(400).json("unable to get user"));
+res.send('Hi Bill, Your code seems to be working')
+  //knex.select("*")
+   // .from("users")
+    //.then( user => res.json(user))
+    //.catch( _ => res.status(400).json("unable to get user"));

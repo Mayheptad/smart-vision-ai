63420e745bfa55318c0939cb908e22b2ab9b6f8c
mayheptad <wfoluwarichie@gmail.com>
2021-11-27 21:32:45 +0100

    add ssl connection to database

--- dbConn.js
+++ dbConn.js
@@ -2,9 +2,10 @@ exports.dbConn = {
   client: "pg",
   connection: {
   connectionString: process.env.DATABASE_URL,
-  ssl: {
+  ssl: true
+ /*  ssl: {
     rejectUnauthorized: false
-  }
+  } */
 /*     host: "127.0.0.1",
     user: "postgres",
     password: "postgres",
